f0ad07ac09e200e61b91d49893172998a9ee50fa
Bhuvanesh Ganesan <bhuvanesh@mallow-tech.com>
2024-01-10 16:35:30 +0530

    version - 0.1.1 update.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,3 +3,7 @@
 ## [0.1.0] - 2024-01-10
 
 - Initial release
+
+## [0.1.1] - 2024-01-10
+
+- Initial gem setup
--- lib/rails_env_helper.rb
+++ lib/rails_env_helper.rb
@@ -4,6 +4,10 @@ require_relative "rails_env_helper/version"
 
 module RailsEnvHelper
   def self.get_env(variable_name)
-    Rails.env.production? ? ENV[variable_name] : Rails.application.credentials[variable_name]
+    if Rails.env.production?
+      ENV[variable_name]
+    else
+      Rails.application.credentials[variable_name]
+    end
   end
 end
